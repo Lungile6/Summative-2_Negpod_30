bd6ffbcf79fae3770cb77736510acc7178f94b9b
Lungile <l.mabelebel@alustudent.com>
2024-02-16 03:35:36 -0800

    Updating main.sh

--- negpod_30-q1/main.sh
+++ negpod_30-q1/main.sh
@@ -1,64 +1,48 @@
 #!/bin/bash
 
-Student_record="students-list_1023.txt"
-
-#creating a student record
-create_student_record() { echo "Enter student details:"
-read -p "Student ID:" student_ID
-read -p "Student name:" student_name
-read -p "Student age:" student_age
-read -p "Student email:" student_email
-
-echo "$student_ID,$student_name,$student_age,$student_email" >> Student_record
-echo "Student has been successfully registered" }
-}
-#Viewing all student details
-view_all_students() { 
-    if [ -s "Student_record" ]; 
-        then  
-            echo "Student ID | Name | Age| Email"
-            echo "                              "
-            cat "$Student_record" | while IFS=, read -r id name age email; do
-            echo "$id | $name | $age | $email"
-        done 
-    else
-    echo "No student registered"
-    fi
-}
-
-#deleting the student record by ID
-delete_student_record() { 
-    read -p "Enter your Student ID" delete_ID
-    sed -i "/^$delete_id,/d" "$Student_record"
-    echo "Student with ID $delete_id has been deleted."
-}
-
-#Updating student record by ID
-
-update_student_record() { 
-    read -p "Enter your Student ID" update_ID
-    sed -i "/^$update_ID,/d" "Student_record"
-    create_student_record
-}
-
-#Home
-
 while true; do
-echo "ALU registration system"
-echo "1. Create Student Record"
-echo "2. View All Students"
-echo "3. Delete Student Record"
-echo "4. Update Student Record"
-echo "5. Exit"
+    echo "1. Create student record"
+    echo "2. View all students"
+    echo "3. Delete student by ID"
+    echo "4. Update student record by ID"
+    echo "5. Exit"
+    read choice
 
-read -p "Select from the drop down" Choice
+    case $choice in
+        1)
+            echo "Enter student email:"
+            read email
+            echo "Enter student age:"
+            read age
+            echo "Enter student ID:"
+            read student_id
 
-case $Choice in
-1) create_student_record ;;
-2) view_all_students ;;
-3) delete_student_record ;;
-4) update_student_record ;;
-5) echo " Exiting the ALU registration system, Thank you!"; exit 0 ;;
-*) echo "Invalid choice, Please enter a valid choice." ;;
-esac
+            echo "$email, $age, $student_id" >> students-list_1023.txt
+            ;;
+        2)
+            cat students-list_1023.txt
+            ;;
+        3)
+            echo "Enter student ID to delete:"
+            read delete_id
+            sed -i "/$delete_id/d" students-list_1023.txt
+            ;;
+        4)
+            echo "Enter student ID to update:"
+            read update_id
+            echo "Enter updated email:"
+            read updated_email
+            echo "Enter updated age:"
+            read updated_age
+
+            sed -i "s/$update_id.*,/$updated_email, $updated_age, $update_id/" students-list_1023.txt
+            ;;
+        5)
+            exit
+            ;;
+        *)
+            echo "Invalid option. Please choose again."
+            ;;
+    esac
 done
+
